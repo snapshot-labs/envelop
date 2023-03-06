991fe9275d30e4a3559d55b435296eee5a04f022
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-06 22:12:03 +0400

    Extract the preview into a middleware
    Preview can now be viewed online instead of only just a local file

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -1,7 +1,6 @@
 import { compile } from 'handlebars';
 import sgMail from '@sendgrid/mail';
 import templates from '../templates';
-import preview from './preview';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
@@ -15,19 +14,10 @@ export async function send(id: string, params: any) {
     html: compile(template.html)(params)
   };
 
-  if (process.env.PREVIEW === 'true') {
-    try {
-      const result: string = preview(id, msg);
-      console.log(`preview email generated at ${result}`);
-    } catch (e) {
-      console.log('preview generation failed', e);
-    }
-  } else {
-    try {
-      const result = await sgMail.send(msg);
-      console.log('email sent', result);
-    } catch (e) {
-      console.log('email failed', e);
-    }
+  try {
+    const result = await sgMail.send(msg);
+    console.log('email sent', result);
+  } catch (e) {
+    console.log('email failed', e);
   }
 }
