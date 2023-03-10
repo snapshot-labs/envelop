863cefd6c6cca82bb08aa64e060f9f20246f3a6b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-11 00:21:14 +0400

    refactor: extract default values into a constants file (#11)

--- src/sender.ts
+++ src/sender.ts
@@ -1,9 +1,9 @@
 import { send } from './helpers/mail';
+import constants from './helpers/constants.json';
 import prepare from './templates/summary';
 
 async function start() {
-  const to = 'fabien@bonustrack.co';
-  const address = '0xeF8305E140ac520225DAf050e2f71d5fBcC543e7';
+  const { to, address } = constants.example;
 
   const result = await send(await prepare({ to, address }));
 
