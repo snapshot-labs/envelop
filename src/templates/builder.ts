49343cc6a756a9293f6be1867de9d7686c56806f
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-12 02:37:26 +0400

    Wrap HTML emails inside a layout

--- src/templates/builder.ts
+++ src/templates/builder.ts
@@ -1,10 +1,13 @@
-import { compile } from 'handlebars';
+import fs from 'fs';
+import Handlebars, { compile } from 'handlebars';
 import templates from './';
 import styles from '../helpers/styles.json';
 
 export default function buildMessage(id: string, params: any) {
   const template = templates[id];
   params.styles = styles;
+
+  Handlebars.registerPartial('layout', fs.readFileSync('./src/templates/layout.hbs', 'utf-8'));
 
   return {
     to: params.to,
