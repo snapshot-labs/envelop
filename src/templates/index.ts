f3c5e3f565dc9532ea3d8b1508200d999912fd90
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-13 00:24:09 +0400

    Add preheader support

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -5,12 +5,14 @@ const templates = {
     from: 'Snapshot <notify@snapshot.org>',
     subject: 'Verify your email address',
     text: 'Verify your email by visiting the following link in your browser: {{verifyLink}}',
+    preheader: 'Verify your email to confirm your weekly summary subscription ',
     html: fs.readFileSync('./src/templates/subscribe/index.hbs', 'utf-8')
   },
   summary: {
     from: 'Snapshot <notify@snapshot.org>',
     subject: 'Your weekly Snapshot summary',
     text: "Hi {{ name }} don't miss out on proposals!",
+    preheader: '',
     html: fs.readFileSync('./src/templates/summary/index.hbs', 'utf-8')
   }
 };
