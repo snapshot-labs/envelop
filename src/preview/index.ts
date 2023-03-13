07fbeb39b11ba197104ef0d7751a6d1694909d81
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-13 14:13:53 +0400

    refactor: refactor template previewer to use the new templates builder (#14)

--- src/preview/index.ts
+++ src/preview/index.ts
@@ -1,35 +1,21 @@
 import fs from 'fs';
 import { compile } from 'handlebars';
 import templates from '../templates';
-import { getProposals } from '../helpers/snapshot';
-import styles from '../helpers/styles.json';
+import constants from '../helpers/constants.json';
 
 export default async function preview(req, res) {
-  const template = templates[req.params.template];
+  const params = {
+    to: req.query.to || constants.example.to,
+    address: req.query.address || constants.example.address
+  };
+  const { template } = req.params;
+  let msg;
 
-  if (!template) {
+  if (templates[template]) {
+    msg = await templates[template].prepare(params);
+  } else {
     return res.sendStatus(404);
   }
-
-  const params = {
-    to: req.query.to || 'fabien@bonustrack.co',
-    name: req.query.name || 'Fabien',
-    address: req.query.address || '0xeF8305E140ac520225DAf050e2f71d5fBcC543e7',
-    proposals: {},
-    styles
-  };
-
-  if (req.params.template === 'summary') {
-    params.proposals = await getProposals(params.address);
-  }
-
-  const msg = {
-    to: params.to,
-    from: compile(template.from)(params),
-    subject: compile(template.subject)(params),
-    text: compile(template.text)(params),
-    html: compile(template.html)(params)
-  };
 
   const content = compile(fs.readFileSync('./src/preview/layout.hbs', 'utf-8'))(msg);
 
