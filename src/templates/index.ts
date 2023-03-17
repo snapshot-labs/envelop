77e39d7d90a3154f77096630a5d2990463f3ace8
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-18 00:38:06 +0400

    Move emails text templates into .hbs files

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -6,17 +6,17 @@ const templates = {
   subscribe: {
     from: 'Snapshot <notify@snapshot.org>',
     subject: 'Verify your email address',
-    text: 'Verify your email by visiting the following link in your browser: {{verifyLink}}',
+    text: fs.readFileSync('./src/templates/subscribe/text.hbs', 'utf-8'),
     preheader: 'Verify your email to confirm your weekly summary subscription ',
-    html: fs.readFileSync('./src/templates/subscribe/index.hbs', 'utf-8'),
+    html: fs.readFileSync('./src/templates/subscribe/html.hbs', 'utf-8'),
     prepare: params => prepareSubscribe(params)
   },
   summary: {
     from: 'Snapshot <notify@snapshot.org>',
     subject: 'Your weekly Snapshot summary',
-    text: "Hi {{ name }} don't miss out on proposals!",
+    text: fs.readFileSync('./src/templates/summary/text.hbs', 'utf-8'),
     preheader: '',
-    html: fs.readFileSync('./src/templates/summary/index.hbs', 'utf-8'),
+    html: fs.readFileSync('./src/templates/summary/html.hbs', 'utf-8'),
     prepare: params => prepareSummary(params)
   }
 };
