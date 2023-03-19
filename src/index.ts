828b61cb4e706588f55ee40d4a5c644e16cdf563
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-19 23:38:31 +0400

    style: update and locally host snapshot logo (#26)

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
+app.use(express.static('./public'));
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
 app.use('/preview/:template', preview);
