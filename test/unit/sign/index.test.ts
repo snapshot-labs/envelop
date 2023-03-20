ddaf389eedff7c6ee345363c31efddab0377b3f7
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-20 16:36:13 +0400

    Fix tests

--- test/unit/sign/index.test.ts
+++ test/unit/sign/index.test.ts
@@ -1,11 +1,8 @@
+process.env.WALLET_PRIVATE_KEY = '1c35d78975cadb12e4047a70a38bade91d2fd9d502884785797db3e9148ec5e2';
+
 import { subscribe, verifySubscribe } from '../../../src/sign';
 
 describe('sign', () => {
-  beforeAll(() => {
-    process.env.WALLET_PRIVATE_KEY =
-      '1c35d78975cadb12e4047a70a38bade91d2fd9d502884785797db3e9148ec5e2';
-  });
-
   describe('verifySubscribe', () => {
     const email = 'test@test.com';
     const address = '0x606535Dd25917855384811C9850D00d011EC1Eb8';
