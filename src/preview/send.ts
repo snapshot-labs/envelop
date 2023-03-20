057399cc29e54d318f143f8367462222cd59738b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-20 21:45:53 +0400

    Set signature depending on action type

--- src/preview/send.ts
+++ src/preview/send.ts
@@ -2,13 +2,18 @@ import { send as sendMail } from '../helpers/mail';
 import templates from '../templates';
 import constants from '../helpers/constants.json';
 import { rpcSuccess, rpcError } from '../helpers/utils';
+import { subscribe, unsubscribe } from '../sign';
 
 export default async function send(req, res) {
+  const { template } = req.params;
   const params = {
     to: constants.example.to,
-    address: [constants.example.address]
+    address: [constants.example.address],
+    signature:
+      template === 'subscribe'
+        ? await subscribe(constants.example.to, constants.example.address)
+        : await unsubscribe(constants.example.to, constants.example.address)
   };
-  const { template } = req.params;
   let msg;
 
   if (templates[template]) {
