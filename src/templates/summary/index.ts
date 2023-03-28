0b03392aa0dc06dac53a4cd318621c8ff1b2de2f
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-03-28 06:03:24 +0400

    style: Show date range related to the proposals

--- src/templates/summary/index.ts
+++ src/templates/summary/index.ts
@@ -1,8 +1,38 @@
 import { getProposals } from '../../helpers/snapshot';
 import buildMessage from '../builder';
 
-export default async function prepare(params: any) {
-  const proposals = await getProposals(params.addresses);
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
 
-  return buildMessage('summary', { ...params, proposals });
+  const nthNumber = number => {
+    if (number > 3 && number < 21) return 'th';
+    switch (number % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  };
+
+  return `${month} ${day}${nthNumber(day)}, ${year}`;
+}
+
+export default async function prepare(params: any) {
+  const proposals = await getProposals(params.addresses, params.endDate);
+  const startDate = new Date(params.endDate);
+  startDate.setDate(startDate.getDate() - 7);
+
+  return buildMessage('summary', {
+    ...params,
+    startDate,
+    formattedStartDate: formatDate(startDate),
+    formattedEndDate: formatDate(params.endDate),
+    proposals
+  });
 }
