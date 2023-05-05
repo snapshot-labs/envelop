ef6683cc086cf2a2e723c7bf8d621bd3745eba11
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-05 15:43:23 +0400

    fix: improve and test email validation

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -50,8 +50,13 @@ export async function getUniqueEmails(): Promise<{ email: string }[]> {
   return await db.queryAsync('SELECT DISTINCT email FROM subscribers WHERE verified > 0');
 }
 
+// RFC5322 standard, does support most format, but not all
+// See test files for list of tested formats
 export function isValidEmail(input: string) {
-  return new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, 'gm').test(input);
+  return new RegExp(
+    /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
+    'gi'
+  ).test(input);
 }
 
 export function isValidAddress(input: string) {
