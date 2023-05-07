31c280feff232907b2154c3757172709be361199
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-05-07 20:54:57 +0400

    chore: fix tests

---
+++ test/fixtures/subscriptionPayload.ts
@@ -0,0 +1,6 @@
+export const subscriptionPayload = {
+  email: 'test@snapshot.org',
+  address: '0x91FD2c8d24767db4Ece7069AA27832ffaf8590f3',
+  signature:
+    '0x09bc1bf7e556e1ccc37d9d3d512dd0b2deac1bf3b5c571bd5076036f0c541509089deedb7d7e1779818908db8501a4e1e8fdced674bc9ed870c4b88f669731101c'
+};
