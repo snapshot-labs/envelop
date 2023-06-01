03302ff9360781e53a109379f48984f83c5ccd5e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-06-01 18:24:41 +0400

    fix: fix incorrect webhook auth header name

--- src/rpc.ts
+++ src/rpc.ts
@@ -80,7 +80,7 @@ router.post('/webhook', async (req, res) => {
   const event = body.event.toString();
   const id = body.id.toString().replace('proposal/', '');
 
-  if (req.headers['authenticate'] !== `${process.env.WEBHOOK_AUTH_TOKEN || ''}`) {
+  if (req.headers['Authentication'] !== `${process.env.WEBHOOK_AUTH_TOKEN || ''}`) {
     return rpcError(res, 'UNAUTHORIZED', id);
   }
 
