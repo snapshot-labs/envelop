96e7145c0703946ac9f5829061e3a5e8f2aa52ff
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-06-06 02:16:09 +0400

    fix: fix missing salt params when sending test verification email

--- scripts/send-subscribe.ts
+++ scripts/send-subscribe.ts
@@ -4,14 +4,14 @@ import subscribe from '../src/queues/processors/subscribe';
 
 async function main() {
   if (process.argv.length < 3) {
-    console.error(`Usage: yarn ts-node scripts/send-subscribe.ts [EMAIL] [ADDRESS]`);
+    console.error(`Usage: yarn ts-node scripts/send-subscribe.ts [EMAIL] [ADDRESS] [SALT]`);
     return process.exit(1);
   }
-  const [, , email, address] = process.argv;
+  const [, , email, address, salt] = process.argv;
 
   return await subscribe({
     name: '',
-    data: { email, address }
+    data: { email, address, salt }
   } as Job);
 }
 
