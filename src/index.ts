53f221f5e428c8c7d419bc6bd2fbd7a5916f260b
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-07-01 14:07:20 +0900

    chore: send errors to sentry

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
 import rpc from './rpc';
 import preview from './preview';
 import send from './preview/send';
@@ -10,6 +11,18 @@ import { rpcError } from './helpers/utils';
 
 const app = express();
 const PORT = process.env.PORT || 3006;
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+  ],
+  tracesSampleRate: 0.5
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 startQueue();
 
@@ -27,6 +40,13 @@ app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
 app.use('/', preview);
 app.use('/', send);
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err: any, req: any, res: any) {
+  res.statusCode = 500;
+  res.end(`${res.sentry}\n`);
+});
 
 app.use((_, res) => {
   rpcError(res, 'RECORD_NOT_FOUND', '');
