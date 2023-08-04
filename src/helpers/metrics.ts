efdc013064155676d984e8039401a541adc5221e
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-04 11:50:56 +0900

    chore: init custom metrics from new exported client object directly

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -1,4 +1,4 @@
-import init from '@snapshot-labs/snapshot-metrics';
+import init, { client } from '@snapshot-labs/snapshot-metrics';
 import db from './mysql';
 import { SUBSCRIPTION_TYPE } from '../templates';
 import { mailerQueue } from '../queues';
@@ -6,7 +6,6 @@ import type { Express } from 'express';
 
 export default function initMetrics(app: Express) {
   init(app, {
-    customMetrics,
     normalizedPath: [
       ['^/preview/.*', '/preview/#template'],
       ['^/send/.*', '/send/#template']
@@ -20,49 +19,47 @@ export default function initMetrics(app: Express) {
   });
 }
 
-async function customMetrics(client: any) {
-  new client.Gauge({
-    name: 'subscribers_per_status_count',
-    help: 'Number of subscribers per status',
-    labelNames: ['status'],
-    async collect() {
-      [
-        ['VERIFIED', 'verified > 0'],
-        ['UNVERIFIED', 'verified IS NULL']
-      ].forEach(async function callback(this: any, data: any) {
-        this.set(
-          { status: data[0] },
-          (await db.queryAsync(`SELECT count(*) as count FROM subscribers WHERE ${data[1]}`))[0]
-            .count as any
-        );
-      }, this);
-    }
-  });
+new client.Gauge({
+  name: 'subscribers_per_status_count',
+  help: 'Number of subscribers per status',
+  labelNames: ['status'],
+  async collect() {
+    [
+      ['VERIFIED', 'verified > 0'],
+      ['UNVERIFIED', 'verified IS NULL']
+    ].forEach(async function callback(this: any, data: any) {
+      this.set(
+        { status: data[0] },
+        (await db.queryAsync(`SELECT count(*) as count FROM subscribers WHERE ${data[1]}`))[0]
+          .count as any
+      );
+    }, this);
+  }
+});
 
-  new client.Gauge({
-    name: 'subscribers_per_subscription_count',
-    help: 'Number of subscribers per subscription type',
-    labelNames: ['type'],
-    async collect() {
-      SUBSCRIPTION_TYPE.forEach(async function callback(this: any, type) {
-        this.set(
-          { type },
-          (
-            await db.queryAsync(
-              `SELECT count(*) as count FROM subscribers WHERE verified > 0 AND JSON_CONTAINS(subscriptions, ?) OR subscriptions IS NULL`,
-              JSON.stringify([type])
-            )
-          )[0].count as any
-        );
-      }, this);
-    }
-  });
+new client.Gauge({
+  name: 'subscribers_per_subscription_count',
+  help: 'Number of subscribers per subscription type',
+  labelNames: ['type'],
+  async collect() {
+    SUBSCRIPTION_TYPE.forEach(async function callback(this: any, type) {
+      this.set(
+        { type },
+        (
+          await db.queryAsync(
+            `SELECT count(*) as count FROM subscribers WHERE verified > 0 AND JSON_CONTAINS(subscriptions, ?) OR subscriptions IS NULL`,
+            JSON.stringify([type])
+          )
+        )[0].count as any
+      );
+    }, this);
+  }
+});
 
-  new client.Gauge({
-    name: 'mailing_queued_jobs',
-    help: 'Number of emails in the queue, pending sending',
-    async collect() {
-      this.set(await mailerQueue.count());
-    }
-  });
-}
+new client.Gauge({
+  name: 'mailing_queued_jobs',
+  help: 'Number of emails in the queue, pending sending',
+  async collect() {
+    this.set(await mailerQueue.count());
+  }
+});
