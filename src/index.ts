773a2a819242bfdd5d713825628af950fc88f446
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-09-07 22:48:08 +0900

    fix: remove morgan logs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
-import morgan from 'morgan';
 import compression from 'compression';
 import cors from 'cors';
+import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
 import rpc from './rpc';
 import preview from './preview';
 import send from './preview/send';
 import { start as startQueue, shutdown as shutdownQueue } from './queues';
 import { rpcError } from './helpers/utils';
 import initMetrics from './helpers/metrics';
-import { initLogger, fallbackLogger } from '@snapshot-labs/snapshot-sentry';
 
 const app = express();
 const PORT = process.env.PORT || 3006;
@@ -23,13 +22,6 @@ app.use(express.json({ limit: '4mb' }));
 app.use(express.urlencoded({ limit: '4mb', extended: false }));
 app.use(express.static('./public'));
 app.use(compression());
-app.use(
-  morgan(
-    '[http] [:date[clf]] ' +
-      '":method :url HTTP/:http-version" :status :res[content-length] ' +
-      '":referrer" ":user-agent" - :response-time ms'
-  )
-);
 app.use(cors({ maxAge: 86400 }));
 app.use('/', rpc);
 app.use('/', preview);
