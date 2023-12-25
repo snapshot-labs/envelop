9c3801d9ae7e80b66c811a78583aa8b5381836f6
Wan <495709+wa0x6e@users.noreply.github.com>
2023-12-25 15:31:14 +0800

    chore: log less output on success (#261)

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -7,7 +7,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 export async function send(msg: Message) {
   try {
     const result = await sgMail.send(msg);
-    console.log('[mail] Email sent', result);
+    console.log('[mail] Email sent', result[0].statusCode, result[0].headers['x-message-id']);
   } catch (e) {
     capture(e);
     console.error('[mail] Email failed', e);
