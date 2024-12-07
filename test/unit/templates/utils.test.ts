bfa41f7ee8e9c747d2afc9c5ad4d5defc3fb8149
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2024-12-07 19:41:41 +0800

    fix: update all links to new new domains and v2 format

--- test/unit/templates/utils.test.ts
+++ test/unit/templates/utils.test.ts
@@ -4,32 +4,32 @@ describe('utils', () => {
   describe('linkWithTracker', () => {
     describe('when the link does not have any query params', () => {
       it('appends the tracker query params', () => {
-        expect(linkWithTracker('https://snapshot.org/test')).toEqual(
-          'https://snapshot.org/test?app=envelop'
+        expect(linkWithTracker('https://snapshot.box/test')).toEqual(
+          'https://snapshot.box/test?app=envelop'
         );
       });
     });
 
     describe('with a vuejs link with hash', () => {
       it('appends the tracker query params', () => {
-        expect(linkWithTracker('https://snapshot.org/#/proposal/some-id')).toEqual(
-          'https://snapshot.org/#/proposal/some-id?app=envelop'
+        expect(linkWithTracker('https://snapshot.box/#/proposal/some-id')).toEqual(
+          'https://snapshot.box/#/proposal/some-id?app=envelop'
         );
       });
     });
 
     describe('with a vuejs link with hash and query params', () => {
       it('appends the tracker query params', () => {
-        expect(linkWithTracker('https://snapshot.org/#/proposal/some-id?test=a')).toEqual(
-          'https://snapshot.org/#/proposal/some-id?test=a&app=envelop'
+        expect(linkWithTracker('https://snapshot.box/#/proposal/some-id?test=a')).toEqual(
+          'https://snapshot.box/#/proposal/some-id?test=a&app=envelop'
         );
       });
     });
 
     describe('when the tracker link already exists', () => {
       it('does not append the tracker link', () => {
-        expect(linkWithTracker('https://snapshot.org/#/proposal/some-id?app=envelop')).toEqual(
-          'https://snapshot.org/#/proposal/some-id?app=envelop'
+        expect(linkWithTracker('https://snapshot.box/#/proposal/some-id?app=envelop')).toEqual(
+          'https://snapshot.box/#/proposal/some-id?app=envelop'
         );
       });
     });
